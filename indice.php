e6fdfcc19620d56568c3ffff764d6bb435b83c51
Giampaolo1 <55143439+Giampaolo1@users.noreply.github.com>
2020-06-06 21:36:50 +0200

    HB

--- indice.php
+++ indice.php
@@ -4,6 +4,13 @@
     <meta charset="utf-8">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="script.js"></script>
+    <!-- JS: HANDLEBARS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.0/handlebars.min.js" charset="utf-8"></script>
+    <!-- TEMPLATE: MESSAGE MENU -->
+    <script id="box-template" type="text/x-handlebars-template">
+      <p> [{{idNode}}] {{ level }} {{ iLeft }} {{ iRight }}</p>
+    </script>
+
     <title>Organizational Chart</title>
   </head>
   <body>
--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+// Rappresentazione grafica con HB
 
 function printData(data) {
-  document.getElementById("container").innerHTML=data;
+
+  var target = $("#container");
+
+  var template = $("#box-template").html();
+  var compiled = Handlebars.compile(template);
+
+  for (var i = 0; i < data.length; i++) {
+    var person = data[i];
+    var personHTML = compiled(person);
+    target.append(personHTML);
+  }
 }
+
+// ----------------------------------
+
+// function printData(data) {
+//   document.getElementById("container").innerHTML=data;
+// }
 
 function getData() {
 
